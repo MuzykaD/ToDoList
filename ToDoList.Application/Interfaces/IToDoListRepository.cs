27d9ce137f88894597d1a71adf2be04f6550e3ec
Muzyka Denys <60756275+MuzykaD@users.noreply.github.com>
2025-02-16 19:02:38 +0200

    Implemented basic Domain

---
+++ ToDoList.Application/Interfaces/IToDoListRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDoList.Application.Interfaces;
+
+public interface IToDoListRepository
+{
+    Task<Entities.ToDoList?> GetByIdAsync(Entities.ToDoList toDoList,CancellationToken cancellationToken = default);
+
+    Task<ICollection<Entities.ToDoList>> GetAsync(CancellationToken cancellationToken = default);
+
+    Task CreateAsync(Entities.ToDoList todoList, CancellationToken cancellationToken = default);
+
+    Task UpdateAsync(Entities.ToDoList updatedList, CancellationToken cancellationToken = default);
+
+    Task DeleteAsync(CancellationToken cancellationToken = default);
+}
