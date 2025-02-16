39f70448a3a83f041c0790e74973b5dd08287f07
Muzyka Denys <60756275+MuzykaD@users.noreply.github.com>
2025-02-16 19:06:15 +0200

    Implemented basic Domain

---
+++ ToDoList.Domain/Entities/ToDoList.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDoList.Domain.Entities;
+
+public class ToDoList
+{
+    public string Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string UserId { get; set; }
+
+    public ICollection<string> SharedTo { get; set; } = [];
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
